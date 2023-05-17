4257fb6c2ad200915af1f793c1a2f03638c804bc
Silvana S. Ledesma <113069949+LedesmaSil@users.noreply.github.com>
2023-05-17 20:20:06 -0300

    SEMANA 4 Python

---
+++ Tecnicatura3Py/Persona.sql
@@ -0,0 +1,16 @@
+
+--Consulta información seleccionada
+--SELECT * FROM persona WHERE id_persona in (1, 2, 3)
+
+--Ingresamos un nuevo elemento a la tabla
+--INSERT INTO persona(nombre, apellido, email)VALUES ('Susana', 'Lara', 'slara@mail.com')
+
+--Hacemos la consulta para ver todo la información de la tabla
+--SELECT * FROM persona
+
+--Modificamos
+--UPDATE persona SET nombre = 'Ivonne', apellido = 'Esparza', email = 'iesparza@mail.com' WHERE id_persona=3
+SELECT * FROM persona
+
+--Borrar
+--DELETE FROM persona WHERE id_persona=3
