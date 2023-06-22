7e057ce9049f25661a63a88f1e56fa90b6ce98dc
Silvana S. Ledesma <113069949+LedesmaSil@users.noreply.github.com>
2023-06-21 21:53:53 -0300

    SEMANA 8 Java

---
+++ Tecnicatura3Java/Leccion08/ManejoExcepciones1/src/excepciones/OperacionExcepcion.java
@@ -0,0 +1,9 @@
+
+package excepciones;
+
+public class OperacionExcepcion extends RuntimeException{
+    //constructor
+    public OperacionExcepcion(String mensaje){
+        super(mensaje);
+    }
+}
